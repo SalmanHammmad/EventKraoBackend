f43a50af4451d8ca8661573c3a8858a1fb4770cc
MuhammadSalmann <salmansidd372@gmail.com>
2024-10-09 04:38:58 +0500

    update cors option for cookies

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true, 
+  }));
 app.use(cookieParser());
-app.use(cors());
 app.use('/users', userRoutes);
 app.use('/events', eventRoutes);
 
